f188ab46f084baa73f1b962091655bf94d41ba06
Alok Sahoo <aloksahoo001@gmail.com>
2025-03-29 13:17:15 +0530

    Added vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import {JetBrains_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 //Components
@@ -29,6 +30,7 @@ export default function RootLayout({
         <Header/>
         <StairTransition/>
         <PageTransition>{children}</PageTransition>
+        <SpeedInsights/>
       </body>
     </html>
   );
