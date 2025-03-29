982c4f8d3e2ad39c85f4c6239fa9432f257883df
Alok Sahoo <aloksahoo001@gmail.com>
2025-03-29 13:12:24 +0530

    Initial conflicts resolved

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,12 +1,18 @@
 "client"
 import Link from "next/link";
 import { GitHub, Linkedin, Twitter } from "react-feather";
+interface SocialProps {
+  containerStyles?: string;
+  iconStyles?: string;
+}
+
 const socials = [
   { icon: <GitHub/>, path:""},
   { icon: <Linkedin/>, path:""},
   { icon: <Twitter/>, path:""},
 ]
-const Socials = ({containerStyles, iconStyles}: any) => {
+
+const Socials: React.FC<SocialProps> = ({ containerStyles = "", iconStyles = "" }) => {
   return (
     <div className={containerStyles}>
       {socials.map((item, index) =>{
